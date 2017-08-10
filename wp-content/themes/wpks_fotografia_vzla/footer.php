0786d204b8fb83fed14588b05d9f2f6d96c82317
Diego Carciente <diego.carciente@gmail.com>
2017-08-09 22:41:11 -0400

    subiendo proyecto, HTML integrando con WordPress

---
+++ wp-content/themes/wpks_fotografia_vzla/footer.php
@@ -0,0 +1,17 @@
+				<!-- Footer -->
+					<footer id="footer">
+						<ul class="copyright">
+							<li>&copy; Untitled.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a>.</li>
+						</ul>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="<?php echo get_template_directory_uri()?>/assets/js/jquery.min.js"></script>
+			<script src="<?php echo get_template_directory_uri()?>/assets/js/skel.min.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="<?php echo get_template_directory_uri()?>/assets/js/main.js"></script>
+
+	</body>
+</html>
